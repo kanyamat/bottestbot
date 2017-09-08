aa8842c6b90226387ff1f594647cfb8890321ebc
kanyamat <kanyamat003@gmail.com>
2017-09-08 11:35:12 -0700

    6

--- webb/index.php
+++ webb/index.php
@@ -1,4 +1,10 @@
 <?php
+
+$content = file_get_contents('php://input');
+$data = json_decode($json,true);
+// Parse JSON
+$events = json_decode($content, true);
+
 require_once '../vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -93,3 +99,21 @@ foreach ($events as $event) {
 		$response = $bot->replyMessage($event->getReplyToken(), $outputText);
 	}
 }  
+
+
+	   $url = 'https://api.line.me/v2/bot/message/reply';
+	   $data = [
+	    'replyToken' => $replyToken,
+	    'messages' => [$messages],
+	   ];
+	   $post = json_encode($data);
+	   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	   $ch = curl_init($url);
+	   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	   $result = curl_exec($ch);
+	   curl_close($ch);
+	   echo $result . "\r\n";
