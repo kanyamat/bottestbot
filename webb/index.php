c16b6d0a903703fc7acb66e06b686394ae774d5b
kanyamat <kanyamat003@gmail.com>
2017-09-08 11:37:41 -0700

    aaa

--- webb/index.php
+++ webb/index.php
@@ -1,22 +1,17 @@
 <?php
 
-$content = file_get_contents('php://input');
-$data = json_decode($json,true);
-// Parse JSON
-$events = json_decode($content, true);
-
 require_once '../vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
 $logger = new Logger('LineBot');
 $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
-//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["Bdz6NuSEZzjQnWDbT8wbR9/iGCTTQ3lGjg69nj+RVbo+QeWV7u6cdo4ayWu6Ys9wKmY6DSv6gEihQQctp8JGaJdah8Zey8bk6dsz5hyhtLB3u/xrFkBWbuAIMs7cQxBtEHWNunTtdxHxisiLL4UAWwdB04t89/1O/w1cDnyilFU="]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["7e8774b550b679c94b402200e393a530"]]);
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<Bdz6NuSEZzjQnWDbT8wbR9/iGCTTQ3lGjg69nj+RVbo+QeWV7u6cdo4ayWu6Ys9wKmY6DSv6gEihQQctp8JGaJdah8Zey8bk6dsz5hyhtLB3u/xrFkBWbuAIMs7cQxBtEHWNunTtdxHxisiLL4UAWwdB04t89/1O/w1cDnyilFU=>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<	
-7e8774b550b679c94b402200e393a530>']);
+//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<Bdz6NuSEZzjQnWDbT8wbR9/iGCTTQ3lGjg69nj+RVbo+QeWV7u6cdo4ayWu6Ys9wKmY6DSv6gEihQQctp8JGaJdah8Zey8bk6dsz5hyhtLB3u/xrFkBWbuAIMs7cQxBtEHWNunTtdxHxisiLL4UAWwdB04t89/1O/w1cDnyilFU=>');
+//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<	
+//7e8774b550b679c94b402200e393a530>']);
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
@@ -101,19 +96,3 @@ foreach ($events as $event) {
 }  
 
 
-	   $url = 'https://api.line.me/v2/bot/message/reply';
-	   $data = [
-	    'replyToken' => $replyToken,
-	    'messages' => [$messages],
-	   ];
-	   $post = json_encode($data);
-	   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-	   $ch = curl_init($url);
-	   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	   $result = curl_exec($ch);
-	   curl_close($ch);
-	   echo $result . "\r\n";
