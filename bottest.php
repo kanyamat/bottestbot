1191f16dfeffff201d3a1223d48d43e48ff48c5b
kanyamat <kanyamat003@gmail.com>
2017-09-21 10:23:23 -0700

    222

--- bottest.php
+++ bottest.php
@@ -12,6 +12,7 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 $_msg = $events['events'][0]['message']['text'];
 $user = $events['events'][0]['source']['userId'];
+$replyToken = $event['replyToken'];
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
  // Loop through each event
