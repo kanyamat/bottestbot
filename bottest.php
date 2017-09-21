6c0ad8ef71fef75052b904f59214f149a4fc7277
kanyamat <kanyamat003@gmail.com>
2017-09-21 10:01:47 -0700

    k

--- bottest.php
+++ bottest.php
@@ -214,45 +214,14 @@ if (!is_null($events['events'])) {
                         'text' => $e 
                       ];
   }else {
-    $replyToken = $event['replyToken'];
-    $text = "ฉันไม่เข้าใจค่ะ";
-    $messages = [
-        'type' => 'text',
-        'text' => $text
-      ];
+    // $replyToken = $event['replyToken'];
+    // $text = "ฉันไม่เข้าใจค่ะ";
+    // $messages = [
+    //     'type' => 'text',
+    //     'text' => $text
+    //   ];
 
- // $replyToken = $event['replyToken'];
- //    $text = "คุณสนใจมีผู้ช่วยไหม";
- //        $messages = [
- //               'type' => 'template',
- //                'altText' => 'this is a confirm template',
- //                'template' => [
- //                    'type' => 'confirm',
- //                    'text' => $text ,
- //                    'actions' => [
- //                        [
- //                            'type' => 'message',
- //                            'label' => 'สนใจ',
- //                            'text' => 'สนใจ'
- //                        ],
- //                        [
- //                            'type' => 'message',
- //                            'label' => 'ไม่สนใจ',
- //                            'text' => 'ไม่สนใจ'
- //                        ],
- //                    ]
- //                ]
- //            ]; 
-  }
-
-//     $replyToken = $event['replyToken'];
-//     $text = "ฉันไม่เข้าใจค่ะ";
-//     $messages = [
-//         'type' => 'text',
-//         'text' => $text
-//       ];
-// }
-   $replyToken = $event['replyToken'];
+ $replyToken = $event['replyToken'];
     $text = "คุณสนใจมีผู้ช่วยไหม";
         $messages = [
                'type' => 'template',
@@ -274,6 +243,9 @@ if (!is_null($events['events'])) {
                     ]
                 ]
             ]; 
+  }
+
+
 }
 }
  
