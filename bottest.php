0b824ce6ad86e74d14e0740e5b86bee4588eed87
kanyamat <kanyamat003@gmail.com>
2017-09-21 12:01:41 -0700

    333

--- bottest.php
+++ bottest.php
@@ -226,35 +226,42 @@ if (!is_null($events['events'])) {
       $json= file_get_contents($url);
       $events = json_decode($json, true);
       $title= $events['items'][0]['title'];
-      $link = $events['items'][0]['link'];
-      $link2 = $events['items'][1]['link'];
+      // $link = $events['items'][0]['link'];
+      // $link2 = $events['items'][1]['link'];
+location
+      $messages = [
+          'type' => 'location',
+          'text' => $title
+        ];
 
-     $messages = [
-          'type' => 'template',
-          'altText' => 'template',
-          'template' => [
-              'type' => 'buttons',
-              'title' =>  $x_tra,
-              'text' =>   $title,
-              'actions' => [
-                  [
-                      'type' => 'postback',
-                      'label' => 'good',
-                      'data' => 'value'
-                  ],
-                  [
-                      'type' => 'uri',
-                      'label' => 'ไปยังลิงค์',
-                      'uri' => $link
-                  ],
-      [
-                      'type' => 'uri',
-                      'label' => 'ไปยังลิงค์2',
-                      'uri' => $link2
-                  ]
-              ]
-          ]
-      ];
+  }
+}
+     // $messages = [
+     //      'type' => 'template',
+     //      'altText' => 'template',
+     //      'template' => [
+     //          'type' => 'buttons',
+     //          'title' =>  $x_tra,
+     //          'text' =>   $title,
+     //          'actions' => [
+     //              [
+     //                  'type' => 'postback',
+     //                  'label' => 'good',
+     //                  'data' => 'value'
+     //              ],
+     //              [
+     //                  'type' => 'uri',
+     //                  'label' => 'ไปยังลิงค์',
+     //                  'uri' => $link
+     //              ],
+     //  [
+     //                  'type' => 'uri',
+     //                  'label' => 'ไปยังลิงค์2',
+     //                  'uri' => $link2
+     //              ]
+     //          ]
+     //      ]
+     //  ];
 
 
    }elseif ($event['type'] == 'message' && $event['message']['type'] == 'text'){
