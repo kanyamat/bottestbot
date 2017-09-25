64c8d17cd44b7609d440ad8490130a2008c20375
kanyamat <kanyamat003@gmail.com>
2017-09-25 09:40:04 -0700

    j

--- bottest.php
+++ bottest.php
@@ -69,7 +69,7 @@ if (!is_null($events['events'])) {
                       ];          
   
    
-  }elseif (strpos($_msg, ' ') !== false) {
+  }elseif (strpos($_msg, date("Y")) !== false) {
   
     $birth_years =  str_replace("","", $_msg);
     $curr_years = date("Y"); 
