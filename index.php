c8ac271d73d6ef1ca4ded31c61885fc5041899f2
kanyamat <kanyamat003@gmail.com>
2017-09-25 13:39:08 -0700

    e

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo "Hello LINE BOT";
+$conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
+$dbconn = pg_pconnect($conn_string);
+    $query = "select question from sequents order by id asc limit 4";
+    $result = pg_query($query);
+      while ($row = pg_fetch_row($result)) {
+       echo  $seqcode = $row[0] ;
+       // echo $seqcode1 =   $row[1] ;
+       // echo $seqcode2 =   $row[2] ;
+       // echo $seqcode3 =   $row[3] ;
+      }
