cb478aafefc53dfa0f7bf0a20a11bf4ac994ba7b
kanyamat <kanyamat003@gmail.com>
2017-09-25 10:27:48 -0700

    i

--- bottest.php
+++ bottest.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
 
                  $messages = [
                         'type' => 'text',
-                        'text' => $seqcode.$seqcode1.$seqcode2.$seqcode3
+                        'text' => $seqcode
                       ]; 
   
     //     $messages = [
