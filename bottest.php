fb63807de6235e6de388480a9071a7019289f569
kanyamat <kanyamat003@gmail.com>
2017-09-26 16:20:38 -0700

    5

--- bottest.php
+++ bottest.php
@@ -70,10 +70,34 @@ if (!is_null($events['events'])) {
                         'text' =>  $hello4
                       ];
 
+      $text = "คุณสนใจมีผู้ช่วยไหม";
+          $messages5 = [
+                 'type' => 'template',
+                  'altText' => 'this is a confirm template',
+                  'template' => [
+                      'type' => 'confirm',
+                      'text' => $text ,
+                      'actions' => [
+                          [
+                              'type' => 'message',
+                              'label' => 'สนใจ',
+                              'text' => 'สนใจ'
+                          ],
+                          [
+                              'type' => 'message',
+                              'label' => 'ไม่สนใจ',
+                              'text' => 'ไม่สนใจ'
+                          ]
+                      ]
+                  ]
+              ]; 
+     $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0006','0',NOW(),NOW())") or die(pg_errormessage());         
+
+
          $url = 'https://api.line.me/v2/bot/message/reply';
          $data = [
           'replyToken' => $replyToken,
-          'messages' => [$messages, $messages2, $messages3, $messages4],
+          'messages' => [$messages, $messages2, $messages3, $messages4, $messages5],
          ];
          error_log(json_encode($data));
          $post = json_encode($data);
