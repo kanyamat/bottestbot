3fe57697d7ce0fcbdb0c434e3634de0881ab84e0
kanyamat <kanyamat003@gmail.com>
2017-09-26 11:46:10 -0700

    s

--- bottest.php
+++ bottest.php
@@ -348,6 +348,7 @@ if (!is_null($events['events'])) {
                       ]
                   ]
               ]; 
+     $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0006','0',NOW(),NOW())") or die(pg_errormessage());         
     // $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0006','0',NOW(),NOW())") or die(pg_errormessage());
     // }else{
     //   $replyToken = $event['replyToken'];
