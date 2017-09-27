6e5e502f8eb7ab4e98e779807087a1c2d2cf1a03
kanyamat <kanyamat003@gmail.com>
2017-09-27 08:23:51 -0700

    a

--- bottest.php
+++ bottest.php
@@ -117,27 +117,27 @@ if (!is_null($events['events'])) {
     //                     'type' => 'text',
     //                     'text' => $seqcode
     //                   ];
-      $messages = [
-             'type' => 'template',
-              'altText' => 'this is a confirm template',
-              'template' => [
-                  'type' => 'confirm',
-                  'text' => $text ,
-                  'actions' => [
-                      [
-                          'type' => 'message',
-                          'label' => 'สนใจ',
-                          'text' => 'สนใจ'
-                      ],
-                      [
-                          'type' => 'message',
-                          'label' => 'ไม่สนใจ',
-                          'text' => 'ไม่สนใจ'
-                      ],
-                  ]
-              ]
-          ];
-   $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0006','0',NOW(),NOW())") or die(pg_errormessage());
+   //    $messages = [
+   //           'type' => 'template',
+   //            'altText' => 'this is a confirm template',
+   //            'template' => [
+   //                'type' => 'confirm',
+   //                'text' => $text ,
+   //                'actions' => [
+   //                    [
+   //                        'type' => 'message',
+   //                        'label' => 'สนใจ',
+   //                        'text' => 'สนใจ'
+   //                    ],
+   //                    [
+   //                        'type' => 'message',
+   //                        'label' => 'ไม่สนใจ',
+   //                        'text' => 'ไม่สนใจ'
+   //                    ],
+   //                ]
+   //            ]
+   //        ];
+   // $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0006','0',NOW(),NOW())") or die(pg_errormessage());
   
   }elseif ($event['message']['text'] == "สนใจ" && $seqcode == "0004"  ) {
                $result = pg_query($dbconn,"SELECT seqcode,question FROM sequents WHERE seqcode = '0006'");
