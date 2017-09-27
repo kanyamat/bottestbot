99776171360adb3dc2a1e8cf8dfa6ae1386b0645
kanyamat <kanyamat003@gmail.com>
2017-09-27 08:33:16 -0700

    ss

--- bottest.php
+++ bottest.php
@@ -28,59 +28,59 @@ if (!is_null($events['events'])) {
   if (strpos($_msg, 'hello') !== false || strpos($_msg, 'สวัสดี') !== false || strpos($_msg, 'หวัดดี') !== false) {
       //$replyToken = $event['replyToken'];
       //$text = "คุณสนใจมีผู้ช่วยไหม";
-      $text = "หากคุณสนใจให้ดิฉันเป็นผู้ช่วยอัตโนมัติของคุณ โปรดกดยืนยันด้างล่างด้วยนะคะ";
+      
     //$check_q = pg_query($dbconn,"SELECT question FROM sequents order by id asc limit 4 ");
-    // $check_q = pg_query($dbconn,"SELECT  question FROM sequents WHERE seqcode='0001'");
-    //             while ($row = pg_fetch_row($check_q)) {
-    //               echo $hello = $row[0]; 
-    //             }
+    $check_q = pg_query($dbconn,"SELECT  question FROM sequents WHERE seqcode='0001'");
+                while ($row = pg_fetch_row($check_q)) {
+                  echo $hello = $row[0]; 
+                }
    
-    // $check_q2 = pg_query($dbconn,"SELECT  question FROM sequents WHERE seqcode='0002'");
-    //             while ($row = pg_fetch_row($check_q2)) {
-    //               echo $hello2 = $row[0];        
-    //             }
+    $check_q2 = pg_query($dbconn,"SELECT  question FROM sequents WHERE seqcode='0002'");
+                while ($row = pg_fetch_row($check_q2)) {
+                  echo $hello2 = $row[0];        
+                }
     
-    // $check_q3 = pg_query($dbconn,"SELECT  question FROM sequents WHERE seqcode='0003'");
-    //             while ($row = pg_fetch_row($check_q3)) {
-    //               echo $hello3 = $row[0];       
-    //             }
-   
-    //                 $replyToken = $event['replyToken'];
+    $check_q3 = pg_query($dbconn,"SELECT  question FROM sequents WHERE seqcode='0003'");
+                while ($row = pg_fetch_row($check_q3)) {
+                  echo $hello3 = $row[0];       
+                }
+          $text = "หากคุณสนใจให้ดิฉันเป็นผู้ช่วยอัตโนมัติของคุณ โปรดกดยืนยันด้างล่างด้วยนะคะ";
+                    $replyToken = $event['replyToken'];
                     
-    //                 $messages5 = [
-    //                     'type' => 'text',
-    //                     'text' =>  $hello
-    //                   ];
-    //                  $messages2 = [
-    //                     'type' => 'text',
-    //                     'text' =>  $hello2
-    //                   ];
-    //                 $messages3 = [
-    //                     'type' => 'text',
-    //                     'text' =>  $hello3
-    //                   ];
+                    $messages = [
+                        'type' => 'text',
+                        'text' =>  $hello
+                      ];
+                     $messages2 = [
+                        'type' => 'text',
+                        'text' =>  $hello2
+                      ];
+                    $messages3 = [
+                        'type' => 'text',
+                        'text' =>  $hello3
+                      ];
 
-    //                 $messages4 = [
-    //                        'type' => 'template',
-    //                         'altText' => 'this is a confirm template',
-    //                         'template' => [
-    //                             'type' => 'confirm',
-    //                             'text' => $text ,
-    //                             'actions' => [
-    //                                 [
-    //                                     'type' => 'message',
-    //                                     'label' => 'สนใจ',
-    //                                     'text' => 'สนใจ'
-    //                                 ],
-    //                                 [
-    //                                     'type' => 'message',
-    //                                     'label' => 'ไม่สนใจ',
-    //                                     'text' => 'ไม่สนใจ'
-    //                                 ]
-    //                             ]
-    //                         ]
-    //                     ]; 
-    //  $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0006','0',NOW(),NOW())") or die(pg_errormessage());         
+                    $messages4 = [
+                           'type' => 'template',
+                            'altText' => 'this is a confirm template',
+                            'template' => [
+                                'type' => 'confirm',
+                                'text' => $text ,
+                                'actions' => [
+                                    [
+                                        'type' => 'message',
+                                        'label' => 'สนใจ',
+                                        'text' => 'สนใจ'
+                                    ],
+                                    [
+                                        'type' => 'message',
+                                        'label' => 'ไม่สนใจ',
+                                        'text' => 'ไม่สนใจ'
+                                    ]
+                                ]
+                            ]
+                        ]; 
+     $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0006','0',NOW(),NOW())") or die(pg_errormessage());         
 
 
          $url = 'https://api.line.me/v2/bot/message/reply';
