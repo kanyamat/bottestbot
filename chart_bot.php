3cfa63cad5a9a203988ae366964a755de066c575
kanyamat <kanyamat003@gmail.com>
2017-09-29 15:12:56 +0700

    d

--- chart_bot.php
+++ chart_bot.php
@@ -2,13 +2,13 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
 $dbconn = pg_pconnect($conn_string);
 
-// $check_q = pg_query($dbconn,"SELECT his_preg_week, his_preg_weight FROM history_preg ");
+$check_q = pg_query($dbconn,"SELECT his_preg_week, his_preg_weight FROM history_preg ");
 
-//  while ($row = pg_fetch_row($check_q)) {
-//                   echo $week = $row[0]; 
-//                   echo $weight = $row[1]; 
+ while ($row = pg_fetch_row($check_q)) {
+                  echo $week = $row[0]; 
+                  echo $weight = $row[1]; 
 
-//                 }
+                }
 //       echo $answer1;
 
 
@@ -18,12 +18,12 @@ $check = pg_query($dbconn,"SELECT user_weight FROM user_data  WHERE  user_id = '
                  $result = $row[0];
   
                 } 
-$check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
-                while ($arr= pg_fetch_array($check_q)) {
-                  $arr0 = $arr[0];
-                  echo $arr1 = $arr[1]-$result;
+// $check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
+//                 while ($arr= pg_fetch_array($check_q)) {
+//                   $arr0 = $arr[0];
+//                   echo $arr1 = $arr[1]-$result;
                 
-                } 
+//                 } 
 
 
 
