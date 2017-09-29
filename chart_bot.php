2c52482a5f2c422918c0f723c78bbb31fa75531d
kanyamat <kanyamat003@gmail.com>
2017-09-29 15:10:43 +0700

    z

--- chart_bot.php
+++ chart_bot.php
@@ -2,14 +2,30 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
 $dbconn = pg_pconnect($conn_string);
 
-$check_q = pg_query($dbconn,"SELECT his_preg_week, his_preg_weight FROM history_preg ");
+// $check_q = pg_query($dbconn,"SELECT his_preg_week, his_preg_weight FROM history_preg ");
 
- while ($row = pg_fetch_row($check_q)) {
-                  echo $week = $row[0]; 
-                  echo $weight = $row[1]; 
+//  while ($row = pg_fetch_row($check_q)) {
+//                   echo $week = $row[0]; 
+//                   echo $weight = $row[1]; 
 
-                }
+//                 }
 //       echo $answer1;
+
+
+$check = pg_query($dbconn,"SELECT user_weight FROM user_data  WHERE  user_id = '{$user_id}'  ");
+                while ($row= pg_fetch_row($check)) {
+              
+                 $result = $row[0];
+  
+                } 
+$check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
+                while ($arr= pg_fetch_array($check_q)) {
+                  $arr0 = $arr[0];
+                  echo $arr1 = $arr[1]-$result;
+                
+                } 
+
+
 
 ?>
 <html>
@@ -35,18 +51,18 @@ $check_q = pg_query($dbconn,"SELECT his_preg_week, his_preg_weight FROM history_
       "type": "xy",
       "theme": "none",
       "dataProvider": [ {
-        "ax": $week, //weekของแต่ละweek ในแนว X
-        "ay": $weight,
+        "ax": 1, //weekของแต่ละweek ในแนว X
+        "ay": 0.5,
         "bx": 1,
         "by": 2.2
       }, {
-        "ax": $week,
-        "ay": $weight,
+        "ax": 2,
+        "ay": 1.3,
         "bx": 2,
         "by": 4.9
       }, {
-        "ax": $week,
-        "ay": $weight,
+        "ax": 3,
+        "ay": 2.3,
         "bx": 3,
         "by": 5.1
       }, {
