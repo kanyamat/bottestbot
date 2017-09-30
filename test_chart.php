b35d679d176f19f3a1a9bb808b3bec4920c6aff3
kanyamat <kanyamat003@gmail.com>
2017-09-30 10:35:12 +0700

    e

--- test_chart.php
+++ test_chart.php
@@ -2,6 +2,10 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
 $dbconn = pg_pconnect($conn_string);
 
+if (!$dbconn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 $res_c = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg");
                 while ($row= pg_fetch_array($res_c)) {
                   $result = $row[0];
