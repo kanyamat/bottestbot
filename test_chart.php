dcf3e18c4b343041a5233ff5c418c24811fdceb9
kanyamat <kanyamat003@gmail.com>
2017-09-30 09:26:46 +0700

    s

--- test_chart.php
+++ test_chart.php
@@ -2,7 +2,7 @@
 $conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47 ";
 $dbconn = pg_pconnect($conn_string);
 
-$res_c = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight");
+$res_c = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg");
                 while ($row= pg_fetch_array($res_c)) {
                   $result = $row[0];
                 
@@ -11,7 +11,7 @@ $res_c = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight");
 // $res_c = $mysqli->query($query);
  
 if (!$result) {
-    die('<p><strong style="color:#FF0000">!! Invalid query:</strong> ' . $mysqli->error.'</p>');
+    die('<p><strong style="color:#FF0000">!! Invalid query...:</strong> ' . $mysqli->error.'</p>');
 }
 ?>
 
