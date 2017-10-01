d57c1c94badba8005de5fb7612e453cf003a8dd4
C:\Users\Administrator\Desktop\pybotbot <kanyamat003@gmail.com>
2017-10-01 18:07:17 +0700

    ก

--- test_chart.php
+++ test_chart.php
@@ -10,7 +10,7 @@ $check = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_pr
 $data = array();
                 while ($row= pg_fetch_row($check)) {
                   echo $result = $row[0],$row[1],"</br>";
-                  $data[] = $result;
+                  $data[] = $row;
 
                 } 
 
