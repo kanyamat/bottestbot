d09f89697a71aa9f9f1dae5b4400f1cdab204735
kanyamat <kanyamat003@gmail.com>
2017-10-03 08:53:28 +0700

    a

--- test_chart.php
+++ test_chart.php
@@ -79,17 +79,19 @@ echo json_encode($data);
     var chart = AmCharts.makeChart( "chartdiv", {
       "type": "xy",
       "theme": "none",
-      "dataProvider": [ {
-        "ax": 1, //weekของแต่ละweek ในแนว X
-        "ay": 0.5,
-        "bx": 1,
-        "by": 2.2
-      }, {
-        "ax": 2,
-        "ay": 1.3,
-        "bx": 2,
-        "by": 4.9
-      } ],
+      "dataProvider": $data
+      // [ {
+      //   "ax": 1, //weekของแต่ละweek ในแนว X
+      //   "ay": 0.5,
+      //   "bx": 1,
+      //   "by": 2.2
+      // }, {
+      //   "ax": 2,
+      //   "ay": 1.3,
+      //   "bx": 2,
+      //   "by": 4.9
+      // } ]
+      ,
       "valueAxes": [ {
         "position": "bottom",
         "axisAlpha": 0,
