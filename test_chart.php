6c97141bf2c2e1d64b7966b645e6207752913cd9
kanyamat <kanyamat003@gmail.com>
2017-10-03 08:46:45 +0700

    a

--- test_chart.php
+++ test_chart.php
@@ -58,7 +58,7 @@ echo json_encode($data);
 
 <html>
   <head>
-    <script src="http://www.amcharts.com/lib/3/plugins/dataloader/dataloader.min.js" type="text/javascript"></script>
+
     <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
     <script src="https://www.amcharts.com/lib/3/xy.js"></script>
     <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
@@ -84,9 +84,61 @@ echo json_encode($data);
         "ay": 0.5,
         "bx": 1,
         "by": 2.2
-    //   "dataLoader": {
-    // "url": "data.php"
-  },
+      }, {
+        "ax": 2,
+        "ay": 1.3,
+        "bx": 2,
+        "by": 4.9
+      }, {
+        "ax": 3,
+        "ay": 2.3,
+        "bx": 3,
+        "by": 5.1
+      }, {
+        "ax": 4,
+        "ay": 2.8,
+        "bx": 4,
+        "by": 5.3
+      }, {
+        "ax": 5,
+        "ay": 3.5,
+        "bx": 5,
+        "by": 6.1
+      }, {
+        "ax": 6,
+        "ay": 5.1,
+        "bx": 6,
+        "by": 8.3
+      }, {
+        "ax": 7,
+        "ay": 6.7,
+        "bx": 7,
+        "by": 10.5
+      }, {
+        "ax": 8,
+        "ay": 8,
+        "bx": 8,
+        "by": 12.3
+      }, {
+        "ax": 9,
+        "ay": 8.9,
+        "bx": 9,
+        "by": 14.5
+      }, {
+        "ax": 10,
+        "ay": 9.7,
+        "bx": 10,
+        "by": 15
+      }, {
+        "ax": 11,
+        "ay": 10.4,
+        "bx": 11,
+        "by": 18.8
+      }, {
+        "ax": 12,
+        "ay": 11.7,
+        "bx": 12,
+        "by": 21
       } ],
       "valueAxes": [ {
         "position": "bottom",
@@ -122,9 +174,32 @@ echo json_encode($data);
       }
     } );
     chart.addListener("dataUpdated", zoomChart);
+function zoomChart() {
+    // chart.zoomToDates(new Date(2012, 0, 3), new Date(2012, 0, 11));
 }
-
-
+// generate some random data, quite different range
+function generateChartData() {
+// var myData=[<?php 
+// while($info=pg_fetch_array($data))
+//     echo $info['his_preg_week'].','; 
+// ?>];
+// <?php
+// $data=pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
+// echo myData;
+// ?>
+// var myLabels=[<?php 
+// while($info = pg_fetch_array($data))
+//     echo '"'.$info['his_preg_weight'].'",'; 
+// ?>];
+    var we = "<?php echo $arr0 = "400,300,500"; ?>";
+    var chartData = [];
+        chartData.push({
+        "date": "2012-01-01",
+        "duration": we
+    });    
+    
+    return chartData;
+}
     </script>
   </head>
   <body>
