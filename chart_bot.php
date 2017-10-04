b964ed4d7ded85d54966104c83d1236331906bab
kanyamat <kanyamat003@gmail.com>
2017-10-04 09:22:51 +0700

    b

--- chart_bot.php
+++ chart_bot.php
@@ -14,166 +14,135 @@ $check = pg_query($dbconn,"SELECT user_weight FROM user_data  WHERE  user_id = '
                  $result = $row[0];
   
                 } 
+$a =[];
+$arrayName=[];
 $check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
                 while ($arr= pg_fetch_array($check_q)) {
-                  $arr0 = $arr[0];
-                  echo $arr1 = $arr[1]-$result;
-                
-                } 
+                  $week = $arr[0];
+                  $weight = $arr[1]-$result;
+         
+                  $arrayName[] = array( 'date' => $week,
+                                      'duration'=> $weight);
+                }   
+$data = json_encode($arrayName);
+echo "var data = '$data';";
 ?>
+<!DOCTYPE html>
 <html>
-  <head>
-
-    <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
-    <script src="https://www.amcharts.com/lib/3/xy.js"></script>
-    <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
-    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <script src="https://www.amcharts.com/lib/3/themes/none.js"></script>
-    <div id="chartdiv"></div>			
-
-    <style type="text/css">
-      #chartdiv {
-      	width		: 100%;
-      	height		: 500px;
-      	font-size	: 11px;
-      }							
-    </style>
-
-    <script>
-    var chartData = generateChartData();
-    var chart = AmCharts.makeChart( "chartdiv", {
-      "type": "xy",
-      "theme": "none",
-      "dataProvider": [ {
-        "ax": 1, //weekของแต่ละweek ในแนว X
-        "ay": 0.5,
-        "bx": 1,
-        "by": 2.2
-      }, {
-        "ax": 2,
-        "ay": 1.3,
-        "bx": 2,
-        "by": 4.9
-      }, {
-        "ax": 3,
-        "ay": 2.3,
-        "bx": 3,
-        "by": 5.1
-      }, {
-        "ax": 4,
-        "ay": 2.8,
-        "bx": 4,
-        "by": 5.3
-      }, {
-        "ax": 5,
-        "ay": 3.5,
-        "bx": 5,
-        "by": 6.1
-      }, {
-        "ax": 6,
-        "ay": 5.1,
-        "bx": 6,
-        "by": 8.3
-      }, {
-        "ax": 7,
-        "ay": 6.7,
-        "bx": 7,
-        "by": 10.5
-      }, {
-        "ax": 8,
-        "ay": 8,
-        "bx": 8,
-        "by": 12.3
-      }, {
-        "ax": 9,
-        "ay": 8.9,
-        "bx": 9,
-        "by": 14.5
-      }, {
-        "ax": 10,
-        "ay": 9.7,
-        "bx": 10,
-        "by": 15
-      }, {
-        "ax": 11,
-        "ay": 10.4,
-        "bx": 11,
-        "by": 18.8
-      }, {
-        "ax": 12,
-        "ay": 11.7,
-        "bx": 12,
-        "by": 21
-      } ],
-      "valueAxes": [ {
-        "position": "bottom",
-        "axisAlpha": 0,
-        "dashLength": 1,
-        "id": "x",
-        "title": "Week"
-      // } ],
-        // "valueAxes": [ {
-        // "axisAlpha": 0,
-        // "dashLength": 1,
-        // "position": "left",
-        // "id": "y",
-        // "title": "Y Axis"
-      } ],
-      "startDuration": 1,
-      "graphs": [ {
-        "balloonText": "x:[[x]] y:[[y]]",
+<head>
+  <title></title>
+</head>
+<script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+<script src="https://www.amcharts.com/lib/3/serial.js"></script>
+<script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
+<link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+<script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+<style>
+#chartdiv {
+  width   : 100%;
+  height    : 500px;
+  font-size : 11px;
+}                            
+</style>
+<script type="text/javascript">
+    
+var chartData = generateChartData();
+var chart = AmCharts.makeChart("chartdiv", {
+    "type": "serial",
+    "theme": "light",
+    "marginRight": 80,
+    "dataProvider": chartData,
+    "balloon": {
+        "cornerRadius": 6,
+        "horizontalPadding": 15,
+        "verticalPadding": 10
+    },
+    "valueAxes": [{
+        "position": "left",
+        "title": "weight"
+    }],
+    "graphs": [{
+        "bullet": "square",
+        "bulletBorderAlpha": 1,
+        "bulletBorderThickness": 1,
         "fillAlphas": 0.3,
-        "fillToAxis": "x",
-        "lineAlpha": 1,
-        "xField": "ax",
-        "yField": "ay",
-        "lineColor": "#FCD202"
-      } ],  
-      "marginLeft": 64,
-      "marginBottom": 60,
-      "chartScrollbar": {},
-      "chartCursor": {},
-      "export": {
-        "enabled": true,
-        "position": "bottom-right"
-      }
-    } );
-    chart.addListener("dataUpdated", zoomChart);
+        "fillColorsField": "lineColor",
+        "legendValueText": "[[value]]",
+        "lineColorField": "lineColor",
+        "title": "duration",
+        "valueField": "duration"
+    }],
+    "chartScrollbar": {
+    },
+    "chartCursor": {
+        "categoryBalloonDateFormat": "week",
+        "cursorAlpha": 0,
+        "fullWidth": true
+    },
+    
+    "categoryField": "date",
+    
+    "categoryAxis": {
+        "axisColor": "#555555",
+        "gridAlpha": 0,
+        "gridCount": 50
+    },
+    "export": {
+        "enabled": true
+    }
+});
+chart.addListener("dataUpdated", zoomChart);
 function zoomChart() {
     // chart.zoomToDates(new Date(2012, 0, 3), new Date(2012, 0, 11));
 }
 // generate some random data, quite different range
 function generateChartData() {
-// var myData=[<?php 
-// while($info=pg_fetch_array($data))
-//     echo $info['his_preg_week'].','; 
-// ?>];
-// <?php
-// $data=pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
-// echo myData;
-// ?>
-// var myLabels=[<?php 
-// while($info = pg_fetch_array($data))
-//     echo '"'.$info['his_preg_weight'].'",'; 
-// ?>];
-    var we = "<?php echo $arr0 = "400,300,500"; ?>";
-    var chartData = [];
-        chartData.push({
-        "date": "2012-01-01",
-        "duration": we
-    });    
-    
+   var chartData = <?php 
+$conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
+$dbconn = pg_pconnect($conn_string);
+if (!$dbconn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// $user = $_GET["data"];
+$user = "U2dc636d2cd052e82c29f5284e00f69b9";
+$user_id = pg_escape_string($user);
+ // echo $user_id;
+$check = pg_query($dbconn,"SELECT user_weight FROM user_data  WHERE  user_id = 'Ub840b452d253f3db490dd59507ab78d1'  ");
+                while ($row= pg_fetch_row($check)) {
+              
+                 $result = $row[0];
+  
+                } 
+$arrayName=[];
+$check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = 'Ub840b452d253f3db490dd59507ab78d1' order by 'his_preg_week'*1  ASC  ");
+                while ($arr= pg_fetch_array($check_q)) {
+                  $week = $arr[0];
+                  $weight = $arr[1]-$result;
+         
+                  $arrayName[] = array( 'date' =>  $week ,
+                                       'duration'=> $weight);
+                }    
+echo $data = json_encode($arrayName);
+ ?>;
+    // chartData.push({
+    //     "date": "2012-01-04",
+    //     "duration": 408
+    // }, {
+    //     "date": "2012-02-04",
+    //     "duration": 482
+    // }, {
+    //     "date": "2012-03-04",
+    //     "duration": 562
+    // }, {
+    //     "date": "2012-04-04",
+    //     "duration": 379
+    // });
     return chartData;
 }
-    </script>
-  </head>
-  <body>
-    
-<!-- HTML -->
+</script>
+
+<body>
+
 <div id="chartdiv"></div>
-   <!--  <div id="curve_chart" style="width: 900px; height: 500px"></div> -->
-   
-  </body>
+</body>
 </html>
-
-
