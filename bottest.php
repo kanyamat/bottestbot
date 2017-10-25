e158971b13dd7fb7e9ba92693d32fafa61edb108
kanyamat <kanyamat003@gmail.com>
2017-10-25 15:09:06 +0700

    h

--- bottest.php
+++ bottest.php
@@ -519,7 +519,7 @@ $replyToken = $event['replyToken'];
  }else if (strpos($_msg, 'แพ้ท้อง') !== false) {
     $replyToken = $event['replyToken'];
     $x_tra = str_replace("แพ้ท้อง","", $_msg);
-    $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:gqr4m9bfx0i&key=AIzaSyB5FmzSJk9yrpwHTyJMQSvl7EdjC7asyyU&q='.$x_tra;
+    $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:gqr4m9bfx0i&key=AIzaSyAtvPyVCpD6WNkS4cfqiIWb5-nBEXL9LK8&q='.$x_tra;
    
     $json= file_get_contents($url);
     $events = json_decode($json, true);
