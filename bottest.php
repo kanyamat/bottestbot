ca09e9eba61cd061ee105dd3b34b82af331ca0d8
C:\Users\Administrator\Desktop\pybotbot <kanyamat003@gmail.com>
2017-10-29 23:17:32 +0700

    a

--- bottest.php
+++ bottest.php
@@ -619,17 +619,17 @@ $replyToken = $event['replyToken'];
 
                 [
                     'type' => 'uri',
-                    'label' => '$title',
+                    'label' => $title,
                     'uri' => $link
                 ],
                 [
                     'type' => 'uri',
-                    'label' => '$title2',
+                    'label' => $title2,
                     'uri' => $link2
                 ],
                 [
                     'type' => 'uri',
-                    'label' => '$title3',
+                    'label' => $title3,
                     'uri' => $link3
                 ]
             ]
